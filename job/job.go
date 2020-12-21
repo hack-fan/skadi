249541e22f2c43b2be8f5f5acaa914f52dc50aab
Muninn <hyacinthus@gmail.com>
2020-12-21 22:53:47 +0800

    job service

---
+++ job/job.go
@@ -0,0 +1,11 @@
+package job
+
+import "github.com/hack-fan/serverfan/types"
+
+func (s *service) Pop(sid string) (*types.ServerJob, error) {
+	return nil, nil
+}
+
+func (s *service) Push(job *types.Job) error {
+	return nil
+}
--- job/service.go
+++ job/service.go
@@ -1,5 +1,19 @@
 package job
 
+import "github.com/hack-fan/serverfan/types"
+
 type Service interface {
-	ServerJobPop(sid, jid string)
+	// Pop a job to server
+	Pop(sid string) (*types.ServerJob, error)
+	// Push a job by client
+	Push(job *types.Job) error
+}
+
+type service struct {
+}
+
+// NewService create a job service instance
+func NewService() Service {
+	var s = &service{}
+	return s
 }
