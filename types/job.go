20bda7fa88afbf6cdd01de599ab8d6bdab2bf4e8
Muninn <hyacinthus@gmail.com>
2021-01-07 15:50:10 +0800

    callback

--- types/job.go
+++ types/job.go
@@ -11,11 +11,16 @@ type JobBasic struct {
 // Job will be saved
 type Job struct {
 	JobBasic
-	UserID      string     `json:"user_id" gorm:"type:varchar(20);index:by_user"`
-	Source      SourceType `json:"source" gorm:"type:varchar(10)"`
-	AgentID     string     `json:"agent_id" gorm:"type:varchar(20);index:by_agent"`
-	Status      string     `json:"status" gorm:"type:varchar(10)"` // queuing/sent/expired/succeeded/failed
-	Result      string     `json:"result" gorm:"type:varchar(1024)"`
+	UserID  string `json:"user_id" gorm:"type:varchar(20);index:by_user"`
+	AgentID string `json:"agent_id" gorm:"type:varchar(20);index:by_agent"`
+	// queuing/sent/expired/succeeded/failed
+	Status string `json:"status" gorm:"type:varchar(10)"`
+	// agent returned, job log or other message
+	Result string `json:"result" gorm:"type:varchar(1024)"`
+	// source context, any string defined by source
+	Source string `json:"source" gorm:"type:varchar(255)"`
+	// callback url, will be called after expired/succeeded/failed
+	Callback    string     `json:"callback" gorm:"type:varchar(255)"`
 	CreatedAt   time.Time  `json:"created_at" gorm:"index:by_user;index:by_agent"`
 	SentAt      *time.Time `json:"sent_at"`
 	ExpiredAt   *time.Time `json:"expired_at"`
--- types/user.go
+++ types/user.go
@@ -2,7 +2,7 @@ package types
 
 import "time"
 
-// User only have id and secret, saved in db.
+// User have id and secret, saved in db.
 type User struct {
 	ID        string     `json:"id" gorm:"type:varchar(20)"`
 	Secret    string     `json:"secret" gorm:"type:varchar(20);index"`
