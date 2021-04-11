c3f379476f9f36afef064438b7f99e0a2137f834
Muninn <hyacinthus@gmail.com>
2021-04-12 00:16:22 +0800

    wip: add ev to service

--- service/service.go
+++ service/service.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-redis/redis/v8"
 	"github.com/go-resty/resty/v2"
+	"github.com/hack-fan/skadi/types"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
@@ -15,16 +16,18 @@ type Service struct {
 	db   *gorm.DB
 	rest *resty.Client
 	log  *zap.SugaredLogger
+	ev   types.EventCenter
 }
 
 // New create a job service instance
-func New(kv *redis.Client, db *gorm.DB, rest *resty.Client, log *zap.SugaredLogger) *Service {
+func New(kv *redis.Client, db *gorm.DB, rest *resty.Client, log *zap.SugaredLogger, ev types.EventCenter) *Service {
 	var s = &Service{
 		ctx:  context.Background(),
 		kv:   kv,
 		db:   db,
 		rest: rest,
 		log:  log,
+		ev:   ev,
 	}
 	return s
 }
