e3c676a16afd1ab1abcdcf14f81d156db82d5a3e
Muninn <hyacinthus@gmail.com>
2021-09-14 20:05:53 +0800

    tmp: channel wip...

--- types/channel.go
+++ types/channel.go
@@ -1,11 +1,24 @@
 package types
 
+import "time"
+
+// ChannelType is provider+way, some provider have more than one way to send message
 type ChannelType string
 
-// Channel is the way can send message to users.
+// All channel types
+const (
+	ChannelTypeWechat   ChannelType = "mp"
+	ChannelTypeTelegram ChannelType = "tg"
+	ChannelTypeFeishu   ChannelType = "feishu"
+)
+
+// Channel is the way of sending message to users.
 type Channel struct {
-	ID     string      `json:"id" gorm:"type:varchar(20);primaryKey"`
-	UserID string      `json:"user_id" gorm:"type:varchar(20);index:idx_uid"`
-	Name   string      `json:"name" gorm:"type:varchar(50)"`
-	Type   ChannelType `json:"type" gorm:"type:varchar(10)"`
+	ID        string      `json:"id" gorm:"type:varchar(20);primaryKey"`
+	UserID    string      `json:"user_id" gorm:"type:varchar(20);index:idx_uid"`
+	Name      string      `json:"name" gorm:"type:varchar(50)"`
+	Type      ChannelType `json:"type" gorm:"type:varchar(10)"`
+	Address   string      `json:"address" gorm:"type:varchar(50)"`
+	CreatedAt time.Time   `json:"created_at"`
+	UpdatedAt time.Time   `json:"updated_at"`
 }
